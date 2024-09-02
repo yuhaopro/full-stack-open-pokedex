42c857b95f1afa40e6e8bad5986d82b321fa1657
Zhu Yuhao <yuhao.swe@gmail.com>
2024-09-02 23:15:34 +0800

    fix eslint

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const express = require("express");
+import express from 'express';
 const app = express();
 
 // get the port from env variable
+// eslint-disable-next-line no-undef
 const PORT = process.env.PORT || 5000;
 
-app.use(express.static("dist"));
+app.use(express.static('dist'));
 
 app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
+  // console.log(`server started on port ${PORT}`);
 });
--- test/App.jest.spec.jsx
+++ test/App.jest.spec.jsx
@@ -1,12 +1,12 @@
-import React from 'react'
-import { render, screen } from '@testing-library/react'
-import axiosMock from 'axios'
-import { act } from 'react-dom/test-utils'
-import '@testing-library/jest-dom'
-import { BrowserRouter as Router } from 'react-router-dom'
-import App from '../src/App'
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import axiosMock from 'axios';
+import { act } from 'react-dom/test-utils';
+import '@testing-library/jest-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from '../src/App';
 
-jest.mock('axios')
+jest.mock('axios');
 
 describe('<App />', () => {
   it('fetches data', async () => {
@@ -16,20 +16,20 @@ describe('<App />', () => {
           results: [{ url: 'https://pokeapi.co/api/v2/pokemon/1/', name: 'bulbasaur', id: 1 }]
         }
       }
-    )
+    );
     await act(async () => {
-      render(<Router><App/></Router>)
-    })
-    expect(axiosMock.get).toHaveBeenCalledTimes(1)
-    expect(axiosMock.get).toHaveBeenCalledWith('https://pokeapi.co/api/v2/pokemon/?limit=50')
-  })
+      render(<Router><App/></Router>);
+    });
+    expect(axiosMock.get).toHaveBeenCalledTimes(1);
+    expect(axiosMock.get).toHaveBeenCalledWith('https://pokeapi.co/api/v2/pokemon/?limit=50');
+  });
 
   it('shows error', async () => {
-    axiosMock.get.mockRejectedValueOnce(new Error())
+    axiosMock.get.mockRejectedValueOnce(new Error());
     await act(async () => {
-      render(<Router><App/></Router>)
-    })
-    expect(screen.getByTestId('error')).toBeVisible()
-  })
+      render(<Router><App/></Router>);
+    });
+    expect(screen.getByTestId('error')).toBeVisible();
+  });
 
-})
+});
